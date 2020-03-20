39c1423217702427ad7c84320c998adbbd96a1b8
lizhenchao <474142438@qq.com>
2020-03-20 09:52:25 +0800

    例子切换

--- examples/App.vue
+++ examples/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <suspension-ball @eventEmit="eventEmit" @positionEmit="positionEmit" :zIndex="1002" :distance="{top: 70, left: 10, right: 25, bottom: 10}" :init="{top: 80}"><div class="float_ball">悬浮1</div></suspension-ball>
-    <suspension-ball @eventEmit="eventEmit" @positionEmit="positionEmit" :zIndex="1002" :distance="{top: 10, left: 10, right: 10, bottom: 10}" :init="{top: 10, left: 10}"><div class="float_ball">悬浮2</div></suspension-ball>
+    <suspension-ball @eventEmit="eventEmit" @positionEmit="positionEmit" :zIndex="1002" :distance="{top: 70, left: 10, right: 25, bottom: 10}" :init="{top: 80}"><div class="float-ball-1">悬浮1</div></suspension-ball>
+    <suspension-ball @eventEmit="eventEmit" @positionEmit="positionEmit" :zIndex="1002" :distance="{top: 10, left: 10, right: 10, bottom: 10}" :init="{top: 10, left: 10}"><div class="float-ball-2">悬浮2</div></suspension-ball>
   </div>
 </template>
 
@@ -34,7 +34,7 @@ export default class App extends Vue {
   text-align: center;
   color: #2c3e50;
   
-  .float_ball{
+  .float-ball-1{
     height: 50px;
     width: 50px;
     display:flex;
@@ -45,10 +45,19 @@ export default class App extends Vue {
     border-radius: 15px;
     box-shadow: 0 0 25px #999;
     background-color: #fff;
-    .float_ball_text{
-      width: 100%;
-      font-size:12px;
-    }
+  }
+
+  .float-ball-2{
+    height: 100px;
+    width: 60px;
+    display:flex;
+    flex-wrap: wrap;
+    align-items:center;
+    align-content:center;
+    justify-content: center;
+    border-radius: 35px;
+    box-shadow: 0 0 25px #999;
+    background-color: #fff;
   }
 }
 </style>
